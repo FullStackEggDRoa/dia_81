0bc6928de2a350687dd7c24aed3490f89b18f1ac
Daniel Roa <droa@apgslt.com>
2023-03-17 01:35:05 -0500

    Ej Extra 1 - Avance 2

--- Ejericio_Extra_1/src/ejericio_extra_1/Ejericio_Extra_1.java
+++ Ejericio_Extra_1/src/ejericio_extra_1/Ejericio_Extra_1.java
@@ -4,6 +4,17 @@
  */
 package ejericio_extra_1;
 
+import Entidades.Amarre;
+import Entidades.Dni;
+import Entidades.Puerto;
+import Entidades.Velero;
+import Entidades.Yate;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
 /**
  *
  * @author droa
@@ -13,8 +24,18 @@ public class Ejericio_Extra_1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
+        Puerto deck1 = new Puerto();
+        
+        //PreCarga de Datos para Pruebas
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = format.parse("17/03/2023");
+        deck1.getAlquileres().add(new Amarre("Gustavo Ascazubi", new Dni('F',"1103345302"), new Date(System.currentTimeMillis()), new Date(123, 3, 15), 1, new Yate(150,3, "PBR-0193", 25, 2015)));
+        deck1.getAlquileres().add(new Amarre("Daniel Roa", new Dni('F',"1102988365"), new Date(System.currentTimeMillis()), new Date(123, 2, 28), 3, new Velero(1, "PBR-0193", 15, 2009)));
+        
+        System.out.println("..:: PRECIO FINAL ALQUILERES ::..");
+        deck1.calcularAlquiler();
     }
     
 }
